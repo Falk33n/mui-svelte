39884d584d3b5d41b9dfdd19e1d88d9284763266
Falk33n <tim.falk00@gmail.com>
2025-02-25 20:37:44 +0100

    feat: add SvelteMouseEvent type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -20,3 +20,7 @@ export type WithChildrenAndChild<T = undefined> = {
 };
 
 export type HeadingLevel = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+
+export type SvelteMouseEvent = MouseEvent & {
+  currentTarget: EventTarget & HTMLButtonElement;
+}
